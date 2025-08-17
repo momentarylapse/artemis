f51f7127cb116598625fe6c7e65ba844e1273362
Michael Ankele <michi@lupina.de>
2025-08-17 10:58:10 +0200

    [graph] GridRenderer.mode

--- src/graph/renderer/GridRenderer.cpp
+++ src/graph/renderer/GridRenderer.cpp
@@ -24,22 +24,41 @@ void GridRenderer::draw_win(const yrenderer::RenderParams& params, MultiViewWind
 		Array<vec3> points;
 		if (g->type == data::GridType::Regular) {
 			auto r = *g->regular;
-			for (int i=0; i<=r.nx; i++)
-				for (int j=0; j<=r.ny; j++) {
-					points.add(r.vertex(i, j, 0));
-					points.add(r.vertex(i, j, r.nz));
-				}
-			for (int i=0; i<=r.nx; i++)
-				for (int k=0; k<=r.nz; k++) {
-					points.add(r.vertex(i, 0, k));
-					points.add(r.vertex(i, r.ny, k));
-				}
-			for (int j=0; j<=r.ny; j++)
-				for (int k=0; k<=r.nz; k++) {
-					points.add(r.vertex(0, j, k));
-					points.add(r.vertex(r.nx, j, k));
-				}
+			if (mode() == RenderMode::Outlines) {
+				for (int i=0; i<=r.nx; i+=r.nx)
+					for (int j=0; j<=r.ny; j+=r.ny) {
+						points.add(r.vertex(i, j, 0));
+						points.add(r.vertex(i, j, r.nz));
+					}
+				for (int i=0; i<=r.nx; i+=r.nx)
+					for (int k=0; k<=r.nz; k+=r.nz) {
+						points.add(r.vertex(i, 0, k));
+						points.add(r.vertex(i, r.ny, k));
+					}
+				for (int j=0; j<=r.ny; j+=r.ny)
+					for (int k=0; k<=r.nz; k+=r.nz) {
+						points.add(r.vertex(0, j, k));
+						points.add(r.vertex(r.nx, j, k));
+					}
+			} else {
+				for (int i=0; i<=r.nx; i++)
+					for (int j=0; j<=r.ny; j++) {
+						points.add(r.vertex(i, j, 0));
+						points.add(r.vertex(i, j, r.nz));
+					}
+				for (int i=0; i<=r.nx; i++)
+					for (int k=0; k<=r.nz; k++) {
+						points.add(r.vertex(i, 0, k));
+						points.add(r.vertex(i, r.ny, k));
+					}
+				for (int j=0; j<=r.ny; j++)
+					for (int k=0; k<=r.nz; k++) {
+						points.add(r.vertex(0, j, k));
+						points.add(r.vertex(r.nx, j, k));
+					}
+			}
 		} else {
+			// if (mode() == RenderMode::Outlines) {} TODO
 			const auto vertices = g->vertices();
 			for (const auto& [a, b]: g->edges()) {
 				points.add(vertices[a]);
--- src/graph/renderer/GridRenderer.h
+++ src/graph/renderer/GridRenderer.h
@@ -20,8 +20,14 @@ public:
 
 	void draw_win(const yrenderer::RenderParams& params, MultiViewWindow* win, yrenderer::RenderViewData& rvd);
 
+	enum class RenderMode {
+		Outlines,
+		Full
+	};
+
 	dataflow::Setting<double> line_width{this, "line-width", 1.0, "range=0:99:0.1"};
 	dataflow::Setting<color> _color{this, "color", Gray};
+	dataflow::SettingFromSet<RenderMode> mode{this, "mode", RenderMode::Full, {RenderMode::Outlines, RenderMode::Full}, {"Outlines", "Full"}};
 
 	dataflow::InPort<data::Grid> regular{this, "grid"};
 };
